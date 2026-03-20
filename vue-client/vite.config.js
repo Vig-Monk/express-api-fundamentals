e9ee498d4fe5ccc5a8c51814a8c42033ec66a1e8
Ludwig <ludwigmaingi@gmail.com>
2026-03-20 10:50:11 +0300

    redeploy

--- vue-client/vite.config.js
+++ vue-client/vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  server: {
+  server: mode === 'development' ? {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        // This rewrites the path if your backend doesn't expect the prefix
-        // rewrite: (path) => path.replace(/^\/api/, ''), 
       },
     },
-  },
-})
+  } : {},
+}))
